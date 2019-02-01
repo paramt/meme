01ccb37e43d87b6403ae204babdaead64d60c904
Param Thakkar <pht0104@gmail.com>
2019-01-31 22:41:28 -0500

    Use algorithmic method instead of goal seek
    - add option between amount and percentage in investment return calculator
    
    - add styling to calculators

--- calculator/script.js
+++ calculator/script.js
@@ -1,37 +1,87 @@
-function predictOutcome(){
+function calculateInvestmentReturn(){
   var oldUpvotes = document.getElementById('upvotes').value;
   var newUpvotes = document.getElementById('future_upvotes').value;
   var investment = document.getElementById('investment').value;
   var factor = calculate(newUpvotes, oldUpvotes);
+  var error = 0;
 
-  var investmentReturn = factor*investment;
+  if(displayInAmount()){
+    if(oldUpvotes !== "" && newUpvotes !== "" && investment !== "" && investment > 0){
+      var investmentReturn = Math.round(factor*investment - investment);
+      var upperLimit = Math.round(sigmoid_max(oldUpvotes)*investment) - investment + " MemeCoins";
+      var upperLimitAmount = calculatePoint(sigmoid_max(oldUpvotes), oldUpvotes);
 
-  if(investmentReturn<investment){
-    var result = "You would lose " + Math.round(investment - investmentReturn) + " MemeCoins";
+      if(investmentReturn > 0){
+        investmentReturn = "+" + investmentReturn;
+      }
+
+      investmentReturn = investmentReturn + " MemeCoins";
+    } else if(oldUpvotes !== "" || newUpvotes !== "" || investment !== ""){
+      error = 1;
+    }
+
+  } else {
+    if(oldUpvotes !== "" && newUpvotes !== ""){
+      var investmentReturn = (Math.round((factor-1)*100*100))/100;
+      var upperLimit = (Math.round(sigmoid_max(oldUpvotes)*10000) - 10000)/100 + "%";
+      var upperLimitAmount = calculatePoint(sigmoid_max(oldUpvotes), oldUpvotes);
+
+      if(investmentReturn >= 0){
+        investmentReturn = "+" + investmentReturn;
+      }
+
+      investmentReturn = investmentReturn + "%";
+    } else {
+      error = 1;
+    }
   }
 
-  if(investmentReturn>investment){
-    var result = "You would gain " + Math.round(investmentReturn - investment) + " MemeCoins";
+  if(error == 0){
+    document.getElementById('result').value = "Return: " + investmentReturn;
+    document.getElementById('max').value = "Maximum: +" + upperLimit + " at " + Math.ceil(upperLimitAmount) + " upvotes";
+    document.getElementById('output').style.opacity = "1";
+
+  } else if(error == 1){
+    document.getElementById('result').value = "Incorrect input";
+    document.getElementById('max').value = "Fill out each field first"
+    document.getElementById('output').style.opacity = "1";
+  } else if(error == 2){
+    document.getElementById('result').value = "Incorrect input";
+    document.getElementById('max').value = "Investment must be greater than 0";
+    document.getElementById('output').style.opacity = "1";
   }
 
-  document.getElementById('result').value = result;
-  console.log(investmentReturn);
 }
 
-
-
-
 function calculateBreakEvenPoint(){
-  var upvotes = document.getElementById("upvotes2").value;
+  var upvotes = document.getElementById('upvotes').value;
+  var output = document.getElementById('result');
 
+  if(upvotes < 0){
+    upvotes = 0;
+  }
+
+  if(upvotes>0 && upvotes!==""){
+    output.value = "Break even at " + Math.round(calculatePoint(1, upvotes)*100)/100 + " upvotes";
+  } else if(upvotes===""){
+    output.value = "Enter a number";
+  } else {
+    output.value = "Enter a number greater than 0";
+  }
+
+
+  document.getElementById('output').style.opacity = "1";
+}
+
+/** calculatePoint using goal seek method
+function calculatePoint(point, upvotes){
   var a = parseInt(upvotes);
   var b = parseInt(upvotes);
   var factor = calculate(a, b);
 
-
   if(b<250){
     // If number is small enough, make calculations accurate to 1 decimal place
-    while(factor<1){
+    while(Math.round(factor*10000)/10000<point){
       factor = calculate(a, b);
       console.log("At " + a + " upvotes your investment return factor will be " + factor);
       a=a*10;
@@ -40,20 +90,61 @@ function calculateBreakEvenPoint(){
     }
 
     var result = a;
-
-    document.getElementById('result2').value = result;
-
-  } else {
+    return result;
+  } else if(b<1000) {
     // If number is bigger, round calculations to nearest whole number
-    while(factor<1){
+    while(Math.round(factor*10000)/10000<point){
       factor = calculate(a, b);
       console.log("At " + a + " upvotes your investment return factor will be " + factor);
       a++;
     }
 
     var result = a;
+    return result;
+  } else {
+    // If number is very big, round calculations to the nearest 10
+    while(Math.round(factor*10000)/10000<point){
+      factor = calculate(a, b);
+      console.log("At " + a + " upvotes your investment return factor will be " + factor);
+      a+=10;
+    }
 
-    document.getElementById('result2').value = result;
+    var result = a;
+    return result;
   }
 
+}
+*/
+
+function changeFormat(){
+  if(document.getElementById("amount").checked){
+    document.getElementById("amount-display").style.display = "block";
+  } else {
+    document.getElementById("amount-display").style.display = "none";
+  }
+}
+
+function displayInAmount(){
+  if(document.getElementById("amount").checked){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function calculatePoint(factor, oldNumber){
+  var y = oldNumber;
+  var z = factor;
+
+  // solves x assuming z = 1 (break even point)
+  // var x = -((Math.log((14/(y+10))+0.2)*(y+100))/4) + ((2549/2500)*y) + 10;
+
+  var x = (y*(625*Math.log(((-(5*y*z))/(5*y*z+50*z-6*y-130)) - (50*z)/(5*y*z+50*z-6*y-130))+2549) + 62500 * Math.log(-(5*y*z)/(5*y*z+50*z-6*y-130) - (50*z)/(5*y*z+50*z-6*y-130)) + 25000)/(2500);
+
+  while(isNaN(x)){
+    z = z-0.001;
+    x = (y*(625*Math.log(((-(5*y*z))/(5*y*z+50*z-6*y-130)) - (50*z)/(5*y*z+50*z-6*y-130))+2549) + 62500 * Math.log(-(5*y*z)/(5*y*z+50*z-6*y-130) - (50*z)/(5*y*z+50*z-6*y-130)) + 25000)/(2500);
+  }
+
+  return x;
 }
